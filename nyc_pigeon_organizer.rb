32023aeca14831228a986eaf62b4a230ab6534b4
shirwet Elfeel <eshirwet@yahoo.com>
2020-03-01 14:29:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+ i = 0
+ while i < data.length do 
+   
+ data.each do |key, value|
+   value.each do |value, attribute|
+      attribute.each do |name|
+ if name == nil
+   new_hash[name] = {}
+   new_hash[name][key] =  []
+   
+ 
+ end
+ end
+ end
+end 
+i+=
+end
+new_hash
 end
